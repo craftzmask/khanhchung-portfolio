cb85b6f6deed6c452336f02ec0702e95a7dddcbd
Khanh Chung <khanh.d.chung@gmail.com>
2025-05-27 21:56:08 -0700

    Switch to HashRouter and update homepage in package.json

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import { ThemeProvider } from "@/components/theme-provider"
-import { BrowserRouter as Router } from 'react-router'
+import { HashRouter as Router } from 'react-router'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
